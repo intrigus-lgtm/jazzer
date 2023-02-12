4c8554b7b8741cc48ac064905cf939e7bf4197d7
Fabian Meumertzheim <meumertzheim@code-intelligence.com>
2023-02-12 21:39:16 +0100

    all: Use google-java-format for Java formatting

--- format.sh
+++ format.sh
@@ -2,8 +2,12 @@
 
 set -euo pipefail
 
-# C++ & Java
-find -name '*.cpp' -o -name '*.c' -o -name '*.h' -o -name '*.java' | xargs clang-format-14 -i
+# Java
+# curl -sSLO https://github.com/google/google-java-format/releases/download/v1.15.0/google-java-format-1.15.0-all-deps.jar && chmod a+x google-java-format-1.15.0-all-deps.jar
+find -name '*.java' | xargs google-java-format-1.15.0-all-deps.jar --replace
+
+# C++
+find -name '*.cpp' -o -name '*.c' -o -name '*.h' | xargs clang-format-14 -i
 
 # Kotlin
 # curl -sSLO https://github.com/pinterest/ktlint/releases/download/0.48.0/ktlint && chmod a+x ktlint
